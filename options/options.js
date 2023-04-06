45c64222e84551d6ad6ffa2960b5cff33a734282
scartag <tag@crowdbotics.com>
2023-04-06 19:54:47 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "appscartest15-dev-67755",
-  url: "https://appscartest15-dev-67755.botics.co",
-  api: "https://appscartest15-dev-67755.botics.co/api/v1"
-}
+  "name": "appscartest15-dev-67755",
+  "url": "https://appscartest15-dev-67755.botics.co",
+  "api": "https://appscartest15-dev-67755.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
